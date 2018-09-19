cbf480c241eab4344638fed12c0cc77bf5cb0ce8
apache17 <brgp@live.com>
2018-09-19 00:31:39 -0600

    List

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,30 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable} from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/do'
-import { UiSwitchModule } from 'ngx-ui-switch';
+
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css']
 })
 
-@Injectable()
 export class BodyComponent implements OnInit {
 
-  constructor(private http: HttpClient) { }
+  constructor(private httpService: HttpClient) { }
 
   products: string [];
 
-  _productURL = '../../assets/products.json';   
-
   ngOnInit() {
-    this.http.get(this._productURL).subscribe(prods=>{
-          this.products = prods as string [];
-        }
-      );
+    this.httpService.get('../../assets/products.json').subscribe(
+      data => {
+        this.products = data as string [];
+        console.log(this.products[1]);
+      },
+      (err: HttpErrorResponse) =>{
+        console.log(err.message);
+      }
+    );
   }
 
 }
