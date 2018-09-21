6f151c6a577379e91a25b1ce9a557f1242a12b6b
apache17 <brgp@live.com>
2018-09-21 13:56:06 -0600

    Mobile - In Progress

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -28,8 +28,8 @@ export class BodyComponent implements OnInit {
     this.httpService.get('../../assets/products.json').subscribe(
       data => {
         this.products = data as string [];
-        this.products.map(function(prod){
-          prod["selected"] = true;
+        this.products.map(function(item){
+          item["selected"] = true;
         });
       },
       (err: HttpErrorResponse) =>{
@@ -39,8 +39,8 @@ export class BodyComponent implements OnInit {
   }
 
   check() {
-  	this.products.map(function(prod){
-  		prod["selected"] = true;
+  	this.products.map(function(item){
+  		item["selected"] = true;
   	});
   }
 
